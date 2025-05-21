a37e6209753be07bf5a1f3cd90cb389171b492b4
Mukul Garg <138233867+gargmukul91066@users.noreply.github.com>
2025-05-22 00:45:01 +0530

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,7 @@
 import cv2
+
+def preprocess_image(img_path, size=(128, 128)):
+    image = cv2.imread(img_path)
+    image = cv2.resize(image, size)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image, gray
