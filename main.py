7530aff9a9333e059ca7d2b4b841037e06becafd
Mukul Garg <138233867+gargmukul91066@users.noreply.github.com>
2025-05-22 00:51:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from utils.data_loader import load_data
+from models.train_models import train_and_evaluate
 
+if __name__ == "__main__":
+    csv_path = "data/train.csv"
+    image_dir = "data/images"
+    
+    print("Loading data and extracting features...")
+    X, y, le = load_data(csv_path, image_dir)
+
+    print("Training models...")
+    train_and_evaluate(X, y, le)
