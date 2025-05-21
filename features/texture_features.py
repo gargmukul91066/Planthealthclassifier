9afdf2247dfb14d30a0dc07ae8fbb3aff66f378e
Mukul Garg <138233867+gargmukul91066@users.noreply.github.com>
2025-05-22 00:31:32 +0530

    Update texture_features.py

--- features/texture_features.py
+++ features/texture_features.py
@@ -1,1 +1,17 @@
+import numpy as np
+from skimage.feature import local_binary_pattern, greycomatrix, greycoprops
 
+def extract_texture_features(image_gray):
+    lbp = local_binary_pattern(image_gray, P=8, R=1, method='uniform')
+    (hist, _) = np.histogram(lbp.ravel(), bins=np.arange(0, 10), range=(0, 9))
+    hist = hist.astype("float")
+    hist /= (hist.sum() + 1e-6)
+
+    glcm = greycomatrix(image_gray, [1], [0], 256, symmetric=True, normed=True)
+    contrast = greycoprops(glcm, 'contrast')[0, 0]
+    dissimilarity = greycoprops(glcm, 'dissimilarity')[0, 0]
+    homogeneity = greycoprops(glcm, 'homogeneity')[0, 0]
+    energy = greycoprops(glcm, 'energy')[0, 0]
+    correlation = greycoprops(glcm, 'correlation')[0, 0]
+
+    return np.hstack([hist, contrast, dissimilarity, homogeneity, energy, correlation])
